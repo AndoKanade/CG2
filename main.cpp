c5e6e4118a391a41fb05414a0f04dcb6281d03bc
AndouKanade <k024g0073@g.neec.ac.jp>
2025-07-23 12:13:35 +0900

    できてるやん なんなんあれイランやんちゃんと資料に書いとけよ

--- main.cpp
+++ main.cpp
@@ -2064,18 +2064,6 @@ IDxcIncludeHandler* includeHandler = nullptr;
   CloseWindow(hwnd);
 
 #pragma endregion
-
-#pragma region 解放しているかの確認
-  Microsoft::WRL::ComPtr<IDXGIDebug1> debug;
-  if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug)))) {
-    debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
-    debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
-    debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
-    debug->Release();
-  }
-
-#pragma endregion
-
   CoUninitialize();
 
   return 0;
